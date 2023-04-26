9aa0209552a75335b798f265d64588b5adc7ccc1
Lab User <labuser@ip-172-31-0-25.ap-south-1.compute.internal>
2023-04-26 11:40:18 +0000

    new message added ti the result

--- src/main/java/javadem/javadem/App.java
+++ src/main/java/javadem/javadem/App.java
@@ -10,10 +10,10 @@ public class App
 {
     public static void main( String[] args )
     {
-    	System.out.println("Addition of Two Mubers");
+    	System.out.println("Addition of Two Mumbers demo for Jenkin");
         
         Calculator calc=new Calculator();
         int result=calc.add(100, 200);
-        System.out.println("Addition RESULT :"+result);
+        System.out.println("Addition of 100 + 200 is --->RESULT :"+result);
     }
 }
